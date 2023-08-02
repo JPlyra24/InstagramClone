90aee81649b8f6c3ebc41b5e2f3ddb9359677c51
João Lyra <joaolyra24@gmail.com>
2023-08-01 23:01:58 -0300

    Update Home.js

--- InstagramClone/src/components/screens/Home.js
+++ InstagramClone/src/components/screens/Home.js
@@ -1,10 +1,29 @@
 import React from "react";
-import {Text, View} from 'react-native'
+import {Text, View, StatusBar} from 'react-native'
+import FontAwesome from "react-native-vector-icons/FontAwesome"
+import Feather from "react-native-vector-icons/Feather"
+import Stories from "../screenComponents/Stories";
 
 const Home = () =>{ 
     return (
-        <View>
-            <Text>Home Screen</Text>
+        <View style={{backgroundColor:'white', height: '100%'}}>
+            <StatusBar backgroundColor="white" barStyle="dark-content" animated={true}/>
+            <View style={{
+                justifyContent:'space-between',
+                flexDirection:'row',
+                paddingHorizontal: 15,
+                alignItems: 'center'
+            }}>
+                <FontAwesome name="plus-square-o" style={{fontSize: 24}}/>
+                <Text style={{fontFamily: "lobster", fontSize:25,fontWeight:'500'}}>
+                    Instagram
+                </Text>
+                <Feather name="navigation" style={{fontSize:24}}/>
+            </View>
+
+            <ScrollView>
+                <Stories/>
+            </ScrollView>
         </View>
     )
 }

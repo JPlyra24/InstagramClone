57d93bfc47302671f188e951a2b2f258add6d164
João Lyra <joaolyra24@gmail.com>
2023-08-01 23:54:53 -0300

    Update Home.js

--- InstagramClone/src/components/screens/Home.js
+++ InstagramClone/src/components/screens/Home.js
@@ -1,31 +1,31 @@
-import React from "react";
-import {Text, View, StatusBar} from 'react-native'
-import FontAwesome from "react-native-vector-icons/FontAwesome"
-import Feather from "react-native-vector-icons/Feather"
-import Stories from "../screenComponents/Stories";
+import React from 'react';
+import {View, Text, StatusBar, ScrollView} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+import Stories from '../screenComponents/Stories';
+import Post from '../screenComponents/Post';
 
-const Home = () =>{ 
-    return (
-        <View style={{backgroundColor:'white', height: '100%'}}>
-            <StatusBar backgroundColor="white" barStyle="dark-content" animated={true}/>
-            <View style={{
-                justifyContent:'space-between',
-                flexDirection:'row',
-                paddingHorizontal: 15,
-                alignItems: 'center'
-            }}>
-                <FontAwesome name="plus-square-o" style={{fontSize: 24}}/>
-                <Text style={{fontFamily: "lobster", fontSize:25,fontWeight:'500'}}>
-                    Instagram
-                </Text>
-                <Feather name="navigation" style={{fontSize:24}}/>
-            </View>
+const Home = () => {
+  return (
+    <View style={{backgroundColor: 'white', height: '100%'}}>
+      <StatusBar backgroundColor="white" barStyle="dark-content" animated={true}/>
+      <View style={{justifyContent: 'space-between', flexDirection: 'row', paddingHorizontal: 15, alignItems: 'center'}}>
+        <FontAwesome name="plus-square-o" style={{fontSize: 24}} />
+        <Text style={{fontFamily: 'lobster', fontSize: 25, fontWeight: '500'}}>
+          Instagram
+        </Text>
+        <Feather name="navigation" style={{fontSize: 24}} />
+      </View>
 
-            <ScrollView>
-                <Stories/>
-            </ScrollView>
+      <ScrollView>
+        <Stories />
+        <Post />
+        <View
+          style={{justifyContent: 'center', alignItems: 'center', padding: 20}}>
         </View>
-    )
-}
+      </ScrollView>
+    </View>
+  );
+};
 
-export default Home
+export default Home;

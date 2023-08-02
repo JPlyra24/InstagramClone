4c75ba8c1a00badcdd4fd59d0bc6d5f2a6e44613
João Lyra <joaolyra24@gmail.com>
2023-08-01 22:25:42 -0300

    base

--- InstagramClone/App.js
+++ InstagramClone/App.js
@@ -1,3 +1,19 @@
+import React, { useEffect, useState }from 'react';
+import {Text, View } from 'react-native';
+import Ionic from "react-native-vector-icons/Ionicons"
+import * as Font from 'expo-font';
+import {NavigationContainer} from "@react-navigation/native";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import Home from './src/components/screens/Home';
+import Search from './src/components/screens/Search';
+import Activity from './src/components/screens/Activity';
+import Profile from './src/components/screens/Profile';
+import Reels from './src/components/screens/Reels';
+
+
+
+
 const App = () => {
   const [fontLoaded, setFontLoaded] = useState(false);
 
@@ -10,3 +26,58 @@ const App = () => {
     };
     loadFont();
   }, []);
+
+  const Stack = createNativeStackNavigator();
+
+  const Tab = createBottomTabNavigator();
+
+  const BottomTabScreen = () => {
+    return (
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarHideOnKeyboard: true,
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarStyle: {
+            height: 50,
+          },
+
+          tabBarIcon: ({focused, size, colour}) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = focused ? 'home-sharp' : 'home-outline';
+              size = focused ? size + 8 : size + 2;
+            } else if (route.name === 'Search') {
+              iconName = focused ? 'search' : 'ios-search-outline';
+            } else if (route.name === 'Reels') {
+              iconName = focused
+                ? 'caret-forward-circle'
+                : 'caret-forward-circle-outline';
+            } else if (route.name === 'Activity') {
+              iconName = focused ? 'ios-heart' : 'ios-heart-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'ios-person-circle' : 'ios-person-outline';
+            }
+
+            return <Ionic name={iconName} size={size} color={colour} />;
+          },
+        })}>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Search" component={Search} />
+        <Tab.Screen name="Reels" component={Reels} />
+        <Tab.Screen name="Activity" component={Activity} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    );
+  };
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name= "Bottom" component={BottomTabScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
